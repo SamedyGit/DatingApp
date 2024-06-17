55ddb06712b08ae6f4eabba9c9fded6bb69367a1
Me Samedy <samedyme81@gmail.com>
2024-06-17 13:57:22 +0700

    lession67

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, importProvidersFrom } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Observable, of } from 'rxjs';
-import { User } from '../_models/user';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +11,9 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(public accountService: AccountService) {}
+  constructor(public accountService: AccountService,
+              public toastr: ToastrService
+              ) {}
 
   ngOnInit(): void {
   }
@@ -23,7 +24,7 @@ export class NavComponent implements OnInit {
       next: response => {
         console.log(response);
       },
-      error: error => console.log(error)    
+      error: error => this.toastr.error(error.error)   
     })
   }
   
